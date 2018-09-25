2fbe624738bbd0660de499f4b46353ac75f6f04f
ramushetty <ummadishettyramu@msitprogram.net>
2018-09-25 22:09:29 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

--- ADS-Assignments/m2/CodeCamp_Percolation/Solution.java
+++ ADS-Assignments/m2/CodeCamp_Percolation/Solution.java
@@ -10,18 +10,43 @@
 
 // You can implement the above API to solve the problem
 import java.util.Scanner;
+/**
+ * Class for percolation.
+ */
 class Percolation {
+	/**
+	 * { boolean 2D array }
+	 */
 	private boolean[][] grid;
+	/**
+	 * { size of array }
+	 */
 	private int size;
 	WeightedQuickUnionUF obj;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      size  The size
+	 */
 	Percolation(final int size) {
 		grid = new boolean[size][size];
 		this.size = size;
 		obj = new WeightedQuickUnionUF(size * size + 2);
 	}
+	/**
+	 * { checks percolate or not}.
+	 *
+	 * @return     { return true or false }
+	 */
 	public boolean ispercolate() {
 		return obj.connected(size * size , size * size + 1);
 	}
+	/**
+	 * { opens the block sites }.
+	 *
+	 * @param      row     The row
+	 * @param      column  The column
+	 */
 	public void open(final int row, final int column){
 		if(grid[row][column]){
 			return;
@@ -47,14 +72,33 @@ class Percolation {
 		}
 	
 	}
+	/**
+	 * { converts to single array }.
+	 *
+	 * @param      i     { row }
+	 * @param      j     { column }
+	 *
+	 * @return     { returns true or not }
+	 */
 	public int convert(final int i , final int j) {
 		return i * size + j;
 	}
 }
+/**
+ * Class for solution.
+ */
 public class Solution {
+	/**
+	 * Constructs the object.
+	 */
 	Solution() {
 		//empty.
 	}
+	/**
+	 * { main function }
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int inp = Integer.parseInt(scan.nextLine());
