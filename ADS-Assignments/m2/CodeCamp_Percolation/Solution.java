5e32a0363945d2fdc9c172bd2b7130ca3a8e85fd
ramushetty <ummadishettyramu@msitprogram.net>
2018-09-25 23:17:08 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

--- ADS-Assignments/m2/CodeCamp_Percolation/Solution.java
+++ ADS-Assignments/m2/CodeCamp_Percolation/Solution.java
@@ -14,7 +14,10 @@ class Percolation {
 	 * { size of array }.
 	 */
 	private int size;
-	WeightedQuickUnionUF obj;
+	/**
+	 * weighted quick union object
+	 */
+	private WeightedQuickUnionUF obj;
 	/**
 	 * Constructs the object.
 	 *
@@ -40,29 +43,29 @@ class Percolation {
 	 * @param      column  The column
 	 */
 	public void open(final int row, final int column) {
-		if(grid[row][column]) {
+		if (grid[row][column]) { 
 			return;
 		}
 		grid[row][column] = true;
-		if(row == 0) {
+		if (row == 0) { 
 			obj.union(convert(row, column), size * size);
 		}
-		if(row == size - 1) {
+		if (row == size - 1) { 
 			obj.union(convert(row, column), size * size + 1);
 		}
-		if(row < size - 1 && grid[row + 1][column]) { //bottom element
+		if (row < size - 1 && grid[row + 1][column]) { //bottom element
 			obj.union(convert(row, column), convert(row + 1, column));
 		}
-		if(row > 0 && grid[row - 1][column]) { //top element
+		if (row > 0 && grid[row - 1][column]) { //top element
 			obj.union(convert(row, column), convert(row - 1, column));
 		}
-		if(column > 0 && grid[row][column - 1]) { //left element
+		if (column > 0 && grid[row][column - 1]) { //left element
 			obj.union(convert(row, column), convert(row, column - 1));
 		}
-		if(column < size - 1 && grid[row][column + 1]) { //right element
+		if (column < size - 1 && grid[row][column + 1]) { //right element
 			obj.union(convert(row, column), convert(row, column + 1));
 		}
-	
+
 	}
 	/**
 	 * { converts to single array }.
@@ -98,7 +101,7 @@ public class Solution {
 		while (scan.hasNextLine()) {
 			String[] tokens = scan.nextLine().split(" ");
 			per.open(Integer.parseInt(
-				tokens[0]) - 1, Integer.parseInt(tokens[1]) - 1);
+			             tokens[0]) - 1, Integer.parseInt(tokens[1]) - 1);
 		}
 		System.out.println(per.ispercolate());
 	}
