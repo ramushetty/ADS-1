d2a6c94e308a1ff27574314cfdbb16a509e914d8
ramushetty <ummadishettyramu@msitprogram.net>
2018-09-26 20:33:36 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- ADS1-Assignments/m3/Sorted_Array/Solution.java
+++ ADS1-Assignments/m3/Sorted_Array/Solution.java
@@ -7,10 +7,11 @@ class  Solution {
 		Scanner scan = new Scanner(System.in);
 		int leng1 = scan.nextInt();
 		int leng2 = scan.nextInt();
+		String ast1 = scan.nextLine();
+		String ast2 = scan.nextLine();
+
 		if (leng2 != 0 && leng1 != 0) {
-			String ast1 = scan.nextLine();
 			String[] arr1 = ast1.split(",");
-			String ast2 = scan.nextLine();
 			String[] arr2 = ast2.split(",");
 		} else {
 			if (leng1 == 0) {
