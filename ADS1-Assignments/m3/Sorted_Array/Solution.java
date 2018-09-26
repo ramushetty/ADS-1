c604f6ada2cf166b61070b1ecd2c34b61103cd29
ramushetty <ummadishettyramu@msitprogram.net>
2018-09-26 20:53:15 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- ADS1-Assignments/m3/Sorted_Array/Solution.java
+++ ADS1-Assignments/m3/Sorted_Array/Solution.java
@@ -5,17 +5,19 @@ class  Solution {
 	// }
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int leng1 = scan.nextInt();
-		int leng2 = scan.nextInt();
+		String leng1 = scan.nextLine();
+		String leng2 = scan.nextLine();
 		String ast1 = scan.nextLine();
-		System.out.println(ast1);
 		String ast2 = scan.nextLine();
+
 		System.out.println(ast2);
-		if (leng2 != 0 && leng1 != 0) {
+		System.out.println(ast1);
+
+		if (Integer.parseInt(leng1) != 0 && Integer.parseInt(leng2) != 0) {
 			String[] arr1 = ast1.split(",");
 			String[] arr2 = ast2.split(",");
 		} else {
-			if (leng1 == 0) {
+			if (Integer.parseInt(leng1) == 0) {
 				System.out.println(ast1);
 			} else {
 				System.out.println(ast2);
