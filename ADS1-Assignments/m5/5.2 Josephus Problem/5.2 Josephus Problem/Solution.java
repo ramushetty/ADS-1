eb234ecab4f9dc18d359f2b92c74082f6a9adda3
ramushetty <ummadishettyramu@msitprogram.net>
2018-09-28 22:20:06 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- ADS1-Assignments/m5/5.2 Josephus Problem/5.2 Josephus Problem/Solution.java
+++ ADS1-Assignments/m5/5.2 Josephus Problem/5.2 Josephus Problem/Solution.java
@@ -3,53 +3,56 @@ import java.util.Scanner;
  * Class for josephus.
  */
 class Josephus {
-	Linkedlist list = new Linkedlist();
-	Josephus() {
-		list = new Linkedlist();
-	}
-	/**
-	 * { for creating nodes }.
-	 *
-	 * @param      first  The first
-	 */
-	public void josephus1(final int first) {
-		for (int i = 0; i < first; i++) {
-			list.insertlast(i);
-		}
+    /**
+     * {linked list }.
+     */
+    private Linkedlist list = new Linkedlist();
+    Josephus() {
+        list = new Linkedlist();
+    }
+    /**
+     * { for creating nodes }.
+     *
+     * @param      first  The first
+     */
+    public void josephus1(final int first) {
+        for (int i = 0; i < first; i++) {
+            list.insertlast(i);
+        }
 
-	}
-	/**
-	 * { josephus2 for iterating }.
-	 *
-	 * @param      first   The first
-	 * @param      second  The second
-	 */
-	public void josephus2(final int first, final int second) {
-		Node temp = list.first;
-		int count = 1;
-		String str = "";
-		while (temp != null) {
-			if (count == second) {
-				// System.out.print(temp.data + " ");
-				str += temp.data + " ";
-				list.deletefirst();
-				temp = temp.next;
-				count = 1;
-			}
-			else {
-				list.insertlast(temp.data);
-				temp = temp.next;
-				list.deletefirst();
-				count++;
-			}
-		}
-		String[] tokens = str.split(" ");
-		int i = 0;
-		for (i = 0; i < tokens.length - 1; i++) {
-			System.out.print(tokens[i] + " ");
-		}
-		System.out.println(tokens[i]);
-	}
+    }
+    /**
+     * { josephus2 for iterating }.
+     *
+     * @param      first   The first
+     * @param      second  The second
+     */
+    public void josephus2(final int first, final int second) {
+        Node temp = list.first;
+        int count = 1;
+        String str = "";
+        while (temp != null) {
+            if (count == second) {
+                // System.out.print(temp.data + " ");
+                str += temp.data + " ";
+                list.deletefirst();
+                temp = temp.next;
+                count = 1;
+            }
+            else {
+                list.insertlast(temp.data);
+                temp = temp.next;
+                list.deletefirst();
+                count++;
+            }
+        }
+        String[] tokens = str.split(" ");
+        int i = 0;
+        for (i = 0; i < tokens.length - 1; i++) {
+            System.out.print(tokens[i] + " ");
+        }
+        System.out.println(tokens[i]);
+    }
 
 }
 /**
