7de1a54517561443e3b360654bf46bfbc1c26f11
ramushetty <ummadishettyramu@msitprogram.net>
2018-10-01 18:15:24 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- ADS1-Assignments/m7/7.1 Steque/7.1 Steque/Solution.java
+++ ADS1-Assignments/m7/7.1 Steque/7.1 Steque/Solution.java
@@ -1,68 +1,89 @@
 import java.util.Scanner;
+/**
+ * Class for steque.
+ */
 class Steque {
-	LinkedList list;
-	Steque() {
-		list = new LinkedList();
-	}
-	public void push(int item) {
-		list.insertfirst(item);
-		list.tostring();
+    LinkedList list;
+    Steque() {
+        list = new LinkedList();
+    }
+    /**
+     * { push }.
+     *
+     * @param      item  The item
+     */
+    public void push(int item) {
+        list.insertfirst(item);
+        list.tostring();
 
-	}
-	public void pop() {
-		list.deletefront();
-		list.tostring();
-	}
-	public void enqueue(int item) {
-		list.insertlast(item);
-		list.tostring();
+    }
+    /**
+     * { pop }.
+     */
+    public void pop() {
+        list.deletefront();
+        list.tostring();
+    }
+    /**
+     * { enqueue}.
+     *
+     * @param      item  The item
+     */
+    public void enqueue(int item) {
+        list.insertlast(item);
+        list.tostring();
 
-	}
+    }
 
 
 }
-
+/**
+ * Class for solution.
+ */
 public class Solution {
-	private Solution() {
-		//empty.
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int itr = Integer.parseInt(scan.nextLine());
-		for (int i = 0; i < itr; i++) {
-			Steque steque = new Steque();
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //empty.
+    }
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int itr = Integer.parseInt(scan.nextLine());
+        for (int i = 0; i < itr; i++) {
+            Steque steque = new Steque();
 
-			while (scan.hasNextLine()) {
-				String str = scan.nextLine();
+            while (scan.hasNextLine()) {
+                String str = scan.nextLine();
 
-				if(str.length() == 0) {
-					System.out.println();
-					break;
-				} 
-				if (str.equals("pop")) {
-					steque.pop();
-				} else {
-					String[] tokens = str.split(" ");
-					switch(tokens[0]) {
-						case "push":
-							steque.push(Integer.parseInt(tokens[1]));
-							break;
-						case "enqueue":
-							steque.enqueue(Integer.parseInt(tokens[1]));
-							break;
-						default:
-							break;
-					}
-				}
-				
-			}
-		
-		}
+                if (str.length() == 0) {
+                    System.out.println();
+                    break;
+                }
+                if (str.equals("pop")) {
+                    steque.pop();
+                } else {
+                    String[] tokens = str.split(" ");
+                    switch (tokens[0]) {
+                    case "push":
+                        steque.push(Integer.parseInt(tokens[1]));
+                        break;
+                    case "enqueue":
+                        steque.enqueue(Integer.parseInt(tokens[1]));
+                        break;
+                    default:
+                        break;
+                    }
+                }
 
-	}
+            }
+
+        }
+
+    }
 }
