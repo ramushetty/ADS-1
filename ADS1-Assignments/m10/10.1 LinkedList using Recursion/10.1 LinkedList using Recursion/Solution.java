c4155cd64e9c1a5cfb4c0088996952e7125e7599
ramushetty <ummadishettyramu@msitprogram.net>
2018-10-04 23:04:21 +0530

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-Assignments/m10/10.1 LinkedList using Recursion/10.1 LinkedList using Recursion/Solution.java
+++ ADS1-Assignments/m10/10.1 LinkedList using Recursion/10.1 LinkedList using Recursion/Solution.java
@@ -3,42 +3,44 @@ import java.util.Scanner;
  * Solution class.
  */
 public final class Solution {
-	/**
-	 * empty constructor.
-	 */
-	private Solution() {
-		//empty.
-	}
-	/**
-	 * main function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		LinkedList list = new LinkedList();
-		int size = 0;
-		while (scan.hasNextLine()) {
-			String str = scan.nextLine();
-			String[] tokens = str.split(" ");
-			switch(tokens[0]) {
-				case "insertAt":
-					if (!list.insertAt(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]))) {
-						size++;
-						System.out.println(list.tostring());
-					}
-					break;
-				case "reverse":
-					if (size == 0) {
-						System.out.println("No elements to reverse");
-						break;
-					}
-					list.reverse();
-					System.out.println(list.tostring());
-					break;
-				default:
-					break;
-			}
-		}
-	}
+  /**
+   * empty constructor.
+   */
+  private Solution() {
+    //empty.
+  }
+  /**
+   * main function.
+   *
+   * @param      args  The arguments
+   */
+  public static void main(final String[] args) {
+    Scanner scan = new Scanner(System.in);
+    LinkedList list = new LinkedList();
+    int size = 0;
+    while (scan.hasNextLine()) {
+      String str = scan.nextLine();
+      String[] tokens = str.split(" ");
+      switch (tokens[0]) {
+      case "insertAt":
+        if (!list.insertAt(Integer.parseInt(
+    tokens[1]), Integer.parseInt(tokens[2]))) {
+          size++;
+          System.out.println(list.tostring());
+        }
+        break;
+      case "reverse":
+        if (size == 0) {
+          System.out.println(
+            "No elements to reverse.");
+          break;
+        }
+        list.reverse();
+        System.out.println(list.tostring());
+        break;
+      default:
+        break;
+      }
+    }
+  }
 }
