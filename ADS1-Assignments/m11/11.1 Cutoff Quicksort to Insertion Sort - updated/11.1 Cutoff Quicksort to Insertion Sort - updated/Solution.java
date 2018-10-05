9d7d55da68ab4816df2964a2fa26d70bd243f0cf
ramushetty <ummadishettyramu@msitprogram.net>
2018-10-05 20:54:34 +0530

    "ads1_m11_a1_v2: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS1-Assignments/m11/11.1 Cutoff Quicksort to Insertion Sort - updated/11.1 Cutoff Quicksort to Insertion Sort - updated/Solution.java
+++ ADS1-Assignments/m11/11.1 Cutoff Quicksort to Insertion Sort - updated/11.1 Cutoff Quicksort to Insertion Sort - updated/Solution.java
@@ -3,30 +3,30 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * empty constructor.
-	 */
-	private Solution() {
-		//empty.
-	}
-	/**
-	 * main function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner s = new Scanner(System.in);
-		Quicksort qu = new Quicksort();
-		int n = Integer.parseInt(s.nextLine());
-		for (int i = 0; i < n; i++) {
-			int cutoff = Integer.parseInt(s.nextLine());
-			String[] tokens = s.nextLine().split(" ");
-			if (tokens[0].equals("")) {
-				System.out.println("[]");
-			} else {
-			qu.sort(tokens,cutoff);
-		}
+    /**
+     * empty constructor.
+     */
+    private Solution() {
+        //empty.
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        Quicksort qu = new Quicksort();
+        int n = Integer.parseInt(s.nextLine());
+        for (int i = 0; i < n; i++) {
+            int cutoff = Integer.parseInt(s.nextLine());
+            String[] tokens = s.nextLine().split(" ");
+            if (tokens[0].equals("")) {
+                System.out.println("[]");
+            } else {
+            qu.sort(tokens, cutoff);
+        }
 
-		}
-	}
+        }
+    }
 }
