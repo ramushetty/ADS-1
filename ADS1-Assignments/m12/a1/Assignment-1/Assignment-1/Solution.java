fa65b6b8c54392dd9e05667d9f255849d1f6e859
ramushetty <ummadishettyramu@msitprogram.net>
2018-10-06 12:45:04 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-Assignments/m12/a1/Assignment-1/Assignment-1/Solution.java
+++ ADS1-Assignments/m12/a1/Assignment-1/Assignment-1/Solution.java
@@ -1,14 +1,5 @@
-// import java.util.Scanner;
-// public final class Solution {
-// 	private Solution() {
-// 		//empty.
-// 	}
-// 	public static void main(String[] args) {
-// 		Scanner scan = new Scanner(System.in);
-// 	}
-// }
-import java.util.*;
-class Reservation {
+import java.util.Scanner;
+class Reservation implements Comparable<Reservation>{
 	String name;
 	String dob;
 	int subject1tmarks;
@@ -29,108 +20,58 @@ class Reservation {
 		category = rc;
 	}
 
-	// static Comparator<Reservation> nameComparator = new Comparator<Reservation>(){
-	// 	public int compare(Reservation s1, Reservation s2) {
-	// 		return s1.name.compareTo(s2.name);
-	// 	}
-	// };
-
-	static Comparator<Reservation> totalComparator = new Comparator<Reservation>(){
-		public int compare(Reservation s1, Reservation s2) {
-			if (s1.totalmarks != s2.totalmarks) {
-				return s1.totalmarks - s2.totalmarks;
-			}
-			else if (s1.subject3tmarks != s2.subject3tmarks) {
-				return s1.subject3tmarks - s2.subject3tmarks;
-			}
-			else if (s1.subject3tmarks != s2.subject3tmarks) {
-				return s1.subject2tmarks - s2.subject2tmarks;
-			}
-			else if (s1.subject3tmarks != s2.subject3tmarks) {
-				return s1.subject1tmarks - s2.subject1tmarks;
-			}
-			return 0;
+	public int compareTo(Reservation pl2){
+		if(this.totalmarks < pl2.totalmarks){
+			return 1;
+			
+		}else if(this.totalmarks > pl2.totalmarks){
+			return -1;
+		}else if(this.subject3tmarks < pl2.subject3tmarks){
+			return 1;
+		}else if(this.subject3tmarks > pl2.subject3tmarks){
+			return -1;
+		// }else if(this.name.compareTo(pl2.name) < 0){
+		// 	return 1;
+		// }else if(this.name.compareTo(pl2.name) > 0){
+		// 	return -1;
 		}
-	};
-
-	// static Comparator<Reservation> ratingComparator = new Comparator<Reservation>(){
-	// 	public int compare(Reservation s1, Reservation s2) {
-	// 		return s1.subject3tmarks - s2.subject3tmarks;
-	// 	}
-	// };
-
-	// static Comparator<Reservation> sizeComparator = new Comparator<Reservation>(){
-	// 	public int compare(Reservation s1, Reservation s2) {
-	// 		return s1.size - s2.size;
-	// 	}
-	// };
-
-
-
-}
-
-class Students {
-	Reservation[] items;
-	int size;
-	Students() {
-		items = new Reservation[10];
-		size = 0;
-	}
-
-	public void resize() {
-		items = Arrays.copyOf(items, items.length * 2);
-	}
-
-	public void addItem(Reservation item) {
-		if (size == items.length) {
-			resize();
-		}
-		items[size++] = item;
-	}
-
-	public Reservation[] getItems() {
-		return items;
-	}
-
-	public void tostring() {
-		for (int i = 0; i < size; i++) {
-			System.out.println(items[i].name + "," + items[i].totalmarks + "," + items[i].category);
-		}
+		return 0;
 	}
 }
 
-public class Solution {
+
+
+
+class Solution{
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		Students sobj = new Students();
-		Sort sort = new Sort();
+		
 		int nostudents = scan.nextInt();
 		int noofvacancy = scan.nextInt();
 		int unrescat = scan.nextInt();
 		int bc = scan.nextInt();
 		int sc = scan.nextInt();
 		int st = scan.nextInt();
+		scan.nextLine();
+		Reservation[] rev = new Reservation[nostudents];
 		for (int i = 0; i < nostudents; i++) {
-			String str = scan.nextLine();
-			// System.out.println(str + "==========");
 			String[] tokens = scan.nextLine().split(",");
-			// System.out.println("=========================================");
-			// System.out.println(tokens[0]+  tokens[1] + " "+ tokens[2] + " " +tokens[2]  + " " + tokens[3] + " " + tokens[4] + " " + tokens[5]);
 			int s1 = Integer.parseInt(tokens[2]);
 
 			int s2 = Integer.parseInt(tokens[3]);
 			int s3 = Integer.parseInt(tokens[4]);
 			int tot = Integer.parseInt(tokens[5]);
-			sobj.addItem(new Reservation(tokens[0], tokens[1], s1, s2, s3, tot,tokens[6]));
+			rev[i] = new Reservation(tokens[0], tokens[1], s1, s2, s3, tot,tokens[6]);
 
 		}
-		Reservation[] items = sobj.getItems();
-		sobj.tostring();
-		System.out.println("-------------------------");
-		sort.iSort(items, 0, items.length - 1, Reservation.totalComparator);
-		// sort.sSort(items, Reservation.costComparator);
-		// Merge mer = new Merge();
-		// mer.mSort(items, Reservation.sizeComparator);
-		sobj.tostring();
+		
+		for(int i = 0; i < nostudents; i++){
+			System.out.println(rev[i].name + ","+rev[i].totalmarks +"," + rev[i].category);
+		}
+		Merge.sort(rev);
+		System.out.println();
+		for(int i = 0; i < noofvacancy; i++){
+			System.out.println(rev[i].name + ","+rev[i].totalmarks + "," + rev[i].category);
+		}
 	}
 }
