531c9a2a6f2d52fec9115454822becc7f008e57f
ramushetty <ummadishettyramu@msitprogram.net>
2018-10-09 09:58:06 +0530

    "ads1_m13_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-Assignments/m13/13.1 isMinHeap/13.1 isMinHeap/Solution.java
+++ ADS1-Assignments/m13/13.1 isMinHeap/13.1 isMinHeap/Solution.java
@@ -1,109 +1,121 @@
 import java.util.Scanner;
-import java.util.Arrays;
+/**
+ * Class for minpq.
+ *
+ * @param      <P>   { generics type }
+ */
 class minpq<P extends Comparable<P>> {
-	public void check(P[] arr) {
-		int k = 1;
-		for (int i = 1; i < arr.length / 2; i++) {
-			if (arr[i].compareTo(arr[2 * i]) > 0 || arr[i].compareTo(arr[2 * i + 1]) > 0 ) {
-				System.out.println("false");
-				return;
-			}
+    /**
+     * check function 
+     * takes time complexity of O(N)
+     *
+     * @param      arr   The arr
+     */
+    public void check(P[] arr) {
+        int k = 1;
+        for (int i = 1; i < arr.length / 2; i++) {
+            if (arr[i].compareTo(arr[2 * i]) > 0 || arr[i].compareTo(arr[2 * i + 1]) > 0 ) {
+                System.out.println("false");
+                return;
+            }
 
-		}
-		System.out.println("true");
-		
-	}
+        }
+        System.out.println("true");
+
+    }
 }
 /**
  * class solution.
  */
 public final class Solution {
-	/**
-	 * empty constructor.
-	 */
-	private Solution() {
-		//empty.
-	}
-	/**
-	 * main function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String str = scan.nextLine();
-		switch (str) {
-			case"String":
-				int count = Integer.parseInt(scan.nextLine());
+    /**
+     * empty constructor.
+     */
+    private Solution() {
+        //empty.
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     * with time complexity of O(N^2)
+     */
 
-				for (int i = 0; i < count; i++) {
-					String[] tokens = scan.nextLine().split(",");
-					String[] st = new String[tokens.length + 1];
-					int k =1;
-					for (int j = 0; j < tokens.length; j++) {
-						st[k++] = tokens[j];
-					}
-					// System.out.println(Arrays.toString(st));
-					minpq<String> min= new minpq<String> ();
-					min.check(st);
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        switch (str) {
+        case"String":
+            int count = Integer.parseInt(scan.nextLine());
 
-				}
-				break;
-			case "Integer":
-				count = Integer.parseInt(scan.nextLine());
-				for (int i = 0; i < count; i++) {
-					String[] tokens = scan.nextLine().split(",");
-					Integer[] st = new Integer[tokens.length + 1];
-					int k =1;
-					for (int j = 0; j < tokens.length; j++) {
-						st[k++] = Integer.parseInt(tokens[j]);
-					}
-					// System.out.println(Arrays.toString(st));
-					minpq<Integer> min= new minpq<Integer> ();
-					min.check(st);
-				}
+            for (int i = 0; i < count; i++) {
+                String[] tokens = scan.nextLine().split(",");
+                String[] st = new String[tokens.length + 1];
+                int k = 1;
+                for (int j = 0; j < tokens.length; j++) {
+                    st[k++] = tokens[j];
+                }
+                // System.out.println(Arrays.toString(st));
+                minpq<String> min = new minpq<String> ();
+                min.check(st);
 
-				break;
-			case "Double":
-				count = Integer.parseInt(scan.nextLine());
-				for (int i = 0; i < count; i++) {
-					String[] tokens = scan.nextLine().split(",");
-					Double[] st = new Double[tokens.length + 1];
-					int k =1;
-					for (int j = 0; j < tokens.length; j++) {
-						st[k++] = Double.parseDouble(tokens[j]);
-					}
-					// System.out.println(Arrays.toString(st));
-					minpq<Double> min= new minpq<Double> ();
-					min.check(st);
-				}
-				break;
-			case "Float":
-				count = Integer.parseInt(scan.nextLine());
-				for (int i = 0; i < count; i++) {
-					String[] tokens = scan.nextLine().split(",");
-					if (tokens[0].equals("")) {
-						System.out.println("false");
-					} else {
-						Float[] st = new Float[tokens.length + 1];
-						int k =1;
-						for (int j = 0; j < tokens.length; j++) {
-							st[k++] = Float.parseFloat(tokens[j]);
-						}
-						// System.out.println(Arrays.toString(st));
-						minpq<Float> min= new minpq<Float> ();
-						min.check(st);
+            }
+            break;
+        case "Integer":
+            count = Integer.parseInt(scan.nextLine());
+            for (int i = 0; i < count; i++) {
+                String[] tokens = scan.nextLine().split(",");
+                Integer[] st = new Integer[tokens.length + 1];
+                int k = 1;
+                for (int j = 0; j < tokens.length; j++) {
+                    st[k++] = Integer.parseInt(tokens[j]);
+                }
+                // System.out.println(Arrays.toString(st));
+                minpq<Integer> min = new minpq<Integer> ();
+                min.check(st);
+            }
 
-					}
+            break;
+        case "Double":
+            count = Integer.parseInt(scan.nextLine());
+            for (int i = 0; i < count; i++) {
+                String[] tokens = scan.nextLine().split(",");
+                Double[] st = new Double[tokens.length + 1];
+                int k = 1;
+                for (int j = 0; j < tokens.length; j++) {
+                    st[k++] = Double.parseDouble(tokens[j]);
+                }
+                // System.out.println(Arrays.toString(st));
+                minpq<Double> min = new minpq<Double> ();
+                min.check(st);
+            }
+            break;
+        case "Float":
+            count = Integer.parseInt(scan.nextLine());
+            for (int i = 0; i < count; i++) {
+                String[] tokens = scan.nextLine().split(",");
+                if (tokens[0].equals("")) {
+                    System.out.println("false");
+                } else {
+                    Float[] st = new Float[tokens.length + 1];
+                    int k = 1;
+                    for (int j = 0; j < tokens.length; j++) {
+                        st[k++] = Float.parseFloat(tokens[j]);
+                    }
+                    // System.out.println(Arrays.toString(st));
+                    minpq<Float> min = new minpq<Float> ();
+                    min.check(st);
 
-					
-				}
-				break;
-		}
-	}
+                }
+
+
+            }
+            break;
+        }
+    }
 }
 // if tokens[0].equals("") {
-// 	System.out.println("false");
+//  System.out.println("false");
 // } else {
 
 // }
