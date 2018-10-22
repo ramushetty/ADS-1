62b51e5b863b85847d4344c6c7a0fab667e9a1c5
ramushetty <ummadishettyramu@msitprogram.net>
2018-10-22 19:24:43 +0530

    "ads1_m16_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-Assignments/m16/16.1 BST/Solution.java
+++ ADS1-Assignments/m16/16.1 BST/Solution.java
@@ -1,6 +1,20 @@
 import java.util.Scanner;
+/**
+ * Solution class.
+ */
 public final class Solution {
-	public static void main(String[] args) {
+	/**
+	 * empty constructor.
+	 */
+	private Solution() {
+		//empty.
+	}
+	/**
+	 * main function.
+	 * time complexity of O(N)
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		BST bst = new BST();
 		while (scan.hasNextLine()) {
@@ -19,93 +33,4 @@ public final class Solution {
 		}
 	}
 }
-class Book {
-	private String book;
-	private String author;
-	private String price;
-	Book(String bookk, String authorr, String pricee) {
-		this.book = bookk;
-		this.author = authorr;
-		this.price  = pricee;
 
-	} 
-	public String getbook() {
-		return book;
-	}
-	public String getauthor() {
-		return author;
-	}
-	public String getprice() {
-		return price;
-	}
-	public int compareTo(Book that) {
-		if (this.getbook().compareTo(that.getbook()) > 0) {
-			return 1;
-		} else if (this.getbook().compareTo(that.getbook()) < 0) {
-			return -1;
-		} else {
-			return 0;
-		}
-
-
-	}
-}
-class BST <Key extends Comparable<Key>, Value> {
-	private Node root;
-	private class Node {
-		private Book key;
-		private Value value;
-		private Node left, right;
-		public Node(Book keyy , Value val) {
-		this.key = keyy;
-		this.value = val;
-		this.left = null;
-		this.right = null;
-		}
-	}
-	public BST() {
-		root = null;
-	}
-	public void put(Book key , Value value) {
-
-		root = put(root, key, value);
-	}
-	private Node put(Node x, Book key, Value value) {
-		if (x == null) {
-			return new Node(key, value);
-		}
-		int cmp = key.compareTo(x.key);
-		if (cmp < 0) {
-			x.left = put(x.left, key, value);
-		} else if (cmp > 0) {
-			x.right = put(x.right, key, value);
-		} else {
-			x.value = value;
-		}
-		return x;
-
-	}
-	public Value get(Book key) {
-        return get(root, key);
-    }
-    private Value get(Node x, Book key) {
-        // if (key == null) {
-        // 	System.out.println("empty");
-        // }
-        if (x == null) {
-        	return null;
-        }
-        int cmp = key.compareTo(x.key);
-        if (cmp < 0) {
-        	return get(x.left, key);
-       	}
-        else if (cmp > 0) {
-        	return get(x.right, key);
-        }
-        else {
-        	return x.value;
-        }
-    }
-
-
-}
