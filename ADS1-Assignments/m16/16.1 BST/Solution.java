d1202c33acec24785503ac626d451db39375883a
ramushetty <ummadishettyramu@msitprogram.net>
2018-10-22 19:26:41 +0530

    "ads1_m16_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS1-Assignments/m16/16.1 BST/Solution.java
+++ ADS1-Assignments/m16/16.1 BST/Solution.java
@@ -3,34 +3,35 @@ import java.util.Scanner;
  * Solution class.
  */
 public final class Solution {
-	/**
-	 * empty constructor.
-	 */
-	private Solution() {
-		//empty.
-	}
-	/**
-	 * main function.
-	 * time complexity of O(N)
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		BST bst = new BST();
-		while (scan.hasNextLine()) {
-			String[] tokens = scan.nextLine().split(",");
-			Book book = new Book(tokens[1], tokens[2], tokens[2 + 1]);
-			switch(tokens[0]) {
-				case"put":
-					bst.put(book, tokens[2 + 2]);
-					break;
-				case "get":
-					System.out.println(bst.get(book));
-					break;
-				default:
-					break;
-			}
-		}
-	}
+    /**
+     * empty constructor.
+     */
+    private Solution() {
+        //empty.
+    }
+    /**
+     * main function.
+     * time complexity of O(N)
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        BST bst = new BST();
+        while (scan.hasNextLine()) {
+            String[] tokens = scan.nextLine().split(",");
+            Book book = new Book(
+                tokens[1], tokens[2], tokens[2 + 1]);
+            switch (tokens[0]) {
+                case"put":
+                    bst.put(book, tokens[2 + 2]);
+                    break;
+                case "get":
+                    System.out.println(bst.get(book));
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 }
 
