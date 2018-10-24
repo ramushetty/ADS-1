c4ade2b679c791d22d1d0923334a15f3fddfdcda
ramushetty <ummadishettyramu@msitprogram.net>
2018-10-24 21:03:49 +0530

    "ads1_m15_a1: 4/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS1-Assignments/m15/15.1 BinarySearchSymbolTable/15.1 BinarySearchSymbolTable/Solution.java
@@ -0,0 +1,464 @@
+import java.util.Scanner;
+import java.util.NoSuchElementException;
+/**
+ * Class for binary search st.
+ *
+ * @param      <Key>    The key
+ * @param      <Value>  The value
+ */
+class BSST<Key extends Comparable<Key>, Value> {
+    /**
+     * {Initial Capacity}.
+     */
+    private static final int INIT_CAPACITY = 5;
+    /**
+     * {Key array}.
+     */
+    private Key[] keys;
+    /**
+     * {Value array}.
+     */
+    private Value[] values;
+    /**
+     * {Variable n}.
+     */
+    private int n = 0;
+    /**
+     * Initializes an empty symbol table.
+     * Time complexity of this method is O(1).
+     */
+    BSST() {
+        this(INIT_CAPACITY);
+    }
+    /**
+     * Initializes an empty symbol table with the specified initial capacity.
+     * @param capacity the maximum capacity
+     * Time complexity of this method is O(1).
+     */
+    BSST(final int cap) {
+        keys = (Key[]) new Comparable[cap];
+        values = (Value[]) new Object[cap];
+    }
+    /**
+     * {resize the underlying arrays}.
+     * Time complexity of this method is O(N).
+     * @param      capacity  The capacity
+     */
+    private void resize(final int cap2) {
+        assert cap2 >= n;
+        Key[]   tempk = (Key[])   new Comparable[cap2];
+        Value[] tempv = (Value[]) new Object[cap2];
+        for (int i = 0; i < n; i++) {
+            tempk[i] = keys[i];
+            tempv[i] = values[i];
+        }
+        values = tempv;
+        keys = tempk;
+    }
+    /**
+     * Returns the number of key-value pairs in this symbol table.
+     * Time complexity of this method is O(1).
+     * @return the number of key-value pairs in this symbol table
+     */
+    public int size() {
+        return n;
+    }
+    /**
+     * Returns true if this symbol table is empty.
+     * Time complexity of this method is O(1).
+     * @return {@code true} if this symbol table is empty;
+     * {@code false} otherwise
+     */
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+    /**
+     * Does this symbol table contain the given key?
+     * Time complexity of this method is O(1).
+     * @param  key the key
+     * @return {@code true} if this symbol table contains {@code key} and
+     * {@code false} otherwise
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public boolean contains(final Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException(
+                "argument to contains() is null");
+        }
+        return get(key) != null;
+    }
+    /**
+     * Returns the value associated with the given key in this symbol table.
+     * Time complexity of this method is O(1).
+     * @param  key the key
+     * @return the value associated with the given key
+     * if the key is in the symbol table
+     * and {@code null} if the key is not in the symbol table
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public Value get(final Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException(
+                "argument to get() is null");
+        }
+        if (isEmpty()) {
+            return null;
+        }
+        int i = rank(key);
+        if (i < n && keys[i].compareTo(key) == 0) {
+            return values[i];
+        }
+        return null;
+    }
+    /**
+     * Returns the number of keys in
+     * this symbol table strictly less than {@code key}.
+     * @param  key the key
+     * @return the number of keys in
+     * the symbol table strictly less than {@code key}
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     * Time complexity of this method is O(log(N)).
+     */
+    public int rank(final Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException(
+                "argument to rank() is null");
+        }
+        int lo = 0, hi = n - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            int cmp = key.compareTo(keys[mid]);
+            if (cmp < 0) {
+                hi = mid - 1;
+            } else if (cmp > 0) {
+                lo = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return lo;
+    }
+    /**
+     * Inserts the specified key-value pair
+     * into the symbol table, overwriting the old
+     * value with the new value if the symbol table
+     * already contains the specified key.
+     * Deletes the specified key (and its associated value)
+     * from this symbol table
+     * if the specified value is {@code null}.
+     * @param  key the key
+     * @param  val the value
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     * Time complexity of this method is O(N).
+     */
+    public void put(final Key key, final Value val) {
+        if (key == null) {
+            throw new IllegalArgumentException(
+                "first argument to put() is null");
+        }
+        if (val == null) {
+            delete(key);
+            return;
+        }
+        int i = rank(key);
+        // key is already in table
+        if (i < n && keys[i].compareTo(key) == 0) {
+            values[i] = val;
+            return;
+        }
+        // insert new key-value pair
+        if (n == keys.length) {
+            resize(2 * keys.length);
+        }
+        for (int j = n; j > i; j--) {
+            keys[j] = keys[j - 1];
+            values[j] = values[j - 1];
+        }
+        keys[i] = key;
+        values[i] = val;
+        n++;
+        assert check();
+    }
+    /**
+     * Removes the specified key and associated value from this symbol table
+     * (if the key is in the symbol table).
+     * Time complexity of this method is O(N).
+     * @param  key the key
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public void delete(final Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException(
+                "argument to delete() is null");
+        }
+        if (isEmpty()) {
+            return;
+        }
+        // compute rank
+        int i = rank(key);
+        // key not in table
+        if (i == n || keys[i].compareTo(key) != 0) {
+            return;
+        }
+        for (int j = i; j < n - 1; j++)  {
+            keys[j] = keys[j + 1];
+            values[j] = values[j + 1];
+        }
+        n--;
+        keys[n] = null;  // to avoid loitering
+        values[n] = null;
+        // resize if 1/4 full
+        if (n > 0 && n == keys.length / 2 + 2) {
+            resize(keys.length / 2);
+        }
+        assert check();
+    }
+    /**
+     * Removes the smallest key and associated value from this symbol table.
+     * Time complexity of this method is O(1).
+     * @throws NoSuchElementException if the symbol table is empty
+     */
+    public void deleteMin() {
+        if (isEmpty()) {
+            throw new NoSuchElementException(
+                "Symbol table underflow error");
+        }
+        delete(min());
+    }
+    /**
+     * Removes the largest key and associated value from this symbol table.
+     * Time complexity of this method is O(1).
+     * @throws NoSuchElementException if the symbol table is empty
+     */
+    public void deleteMax() {
+        if (isEmpty()) {
+            throw new NoSuchElementException(
+                "Symbol table underflow error");
+        }
+        delete(max());
+    }
+    /**
+      * Returns the smallest key in this symbol table.
+      * Time complexity of this method is O(1).
+      * @return the smallest key in this symbol table
+      * @throws NoSuchElementException if this symbol table is empty
+      */
+    public Key min() {
+        if (isEmpty()) {
+            throw new NoSuchElementException(
+                "called min() with empty symbol table");
+        }
+        return keys[0];
+    }
+    /**
+     * Returns the largest key in this symbol table.
+     * Time complexity of this method is O(1).
+     * @return the largest key in this symbol table
+     * @throws NoSuchElementException if this symbol table is empty
+     */
+    public Key max() {
+        if (isEmpty()) {
+            throw new NoSuchElementException(
+                "called max() with empty symbol table");
+        }
+        return keys[n - 1];
+    }
+    /**
+     * Return the kth smallest key in this symbol table.
+     * Time complexity of this method is O(1).
+     * @param  k the order statistic
+     * @return the {@code k}th smallest
+     * key in this symbol table
+     * @throws IllegalArgumentException unless {@code k} is between 0 and
+     *        <em>n</em>–1
+     */
+    public Key select(final int k) {
+        if (k < 0 || k >= size()) {
+            throw new IllegalArgumentException(
+                "called select() with invalid argument: " + k);
+        }
+        return keys[k];
+    }
+    /**
+     * Returns the largest key in this
+     * symbol table less than or equal to {@code key}.
+     * @param  key the key
+     * @return the largest key in this symbol
+     * table less than or equal to {@code key}
+     * @throws NoSuchElementException if there is no such key
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     * Time complexity of this method is O(log(N)).
+     */
+    public Key floor(final Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException(
+                "argument to floor() is null");
+        }
+        int i = rank(key);
+        if (i < n && key.compareTo(keys[i]) == 0) {
+            return keys[i];
+        }
+        if (i == 0) {
+            return null;
+        } else {
+            return keys[i - 1];
+        }
+    }
+    /**
+     * Returns the smallest key in this symbol table
+     * greater than or equal to {@code key}.
+     * @param  key the key
+     * @return the smallest key in this symbol table
+     * greater than or equal to {@code key}
+     * @throws NoSuchElementException if there is no such key
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     * Time complexity of this method is O(log(N)).
+     */
+    public Key ceiling(final Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException(
+                "argument to ceiling() is null");
+        }
+        int i = rank(key);
+        if (i == n) {
+            return null;
+        } else {
+            return keys[i];
+        }
+    }
+
+    /**
+     * Returns the number of keys in this symbol table in the specified range.
+     * Time complexity of this method is O(1).
+     * @param lo minimum endpoint
+     * @param hi maximum endpoint
+     * @return the number of keys in this symbol table between {@code lo}
+     *         (inclusive) and {@code hi} (inclusive)
+     * @throws IllegalArgumentException if either {@code lo} or {@code hi}
+     *         is {@code null}
+     */
+    public int size(final Key lo, final Key hi) {
+        if (lo == null) {
+            throw new IllegalArgumentException(
+                "first argument to size() is null");
+        }
+        if (hi == null) {
+            throw new IllegalArgumentException(
+                "second argument to size() is null");
+        }
+        if (lo.compareTo(hi) > 0) {
+            return 0;
+        }
+        if (contains(hi)) {
+            return rank(hi) - rank(lo) + 1;
+        } else {
+            return rank(hi) - rank(lo);
+        }
+    }
+    /**
+     * {Method to check}.
+     * Time complexity of this method is O(1).
+     * @return     {Boolean}
+     */
+    private boolean check() {
+        return isSorted() && rankCheck();
+    }
+    /**
+     * Determines if sorted.
+     * Time complexity of this method is O(N).
+     * @return     True if sorted, False otherwise.
+     */
+    private boolean isSorted() {
+        for (int i = 1; i < size(); i++) {
+            if (keys[i].compareTo(keys[i - 1]) < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    /**
+     * {Method to check the rank}.
+     * Time complexity of this method is O(N^2).
+     * @return     {Boolean}
+     */
+    private boolean rankCheck() {
+        for (int i = 0; i < size(); i++) {
+            if (i != rank(select(i))) {
+                return false;
+            }
+        }
+        for (int i = 0; i < size(); i++) {
+            if (keys[i].compareTo(select(rank(keys[i]))) != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    /**
+     * Returns a string representation of the object.
+     * Time complexity of this method is O(1).
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        String str = "";
+        int i;
+        for (i = 0; i < size() - 1; i++) {
+            str += keys[i] + " " + values[i] + "\n";
+        }
+        str += keys[i] + " " + values[i];
+        return str;
+    }
+}
+/**
+ * solution class.
+ */
+public final class Solution {
+    /**
+     * empty.
+     */
+    private Solution() {
+        //empty constructor.
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        BSST<String, Integer> st;
+        st = new BSST<String, Integer>();
+        String[] tokens = scan.nextLine().split(" ");
+        for (int i = 0; i < tokens.length; i++) {
+            st.put(tokens[i], i);
+        }
+        while (scan.hasNextLine()) {
+            String[] split = scan.nextLine().split(" ");
+            switch (split[0]) {
+            case "max":
+                System.out.println(st.max());
+                break;
+            case "floor":
+                System.out.println(st.floor(split[1]));
+                break;
+            case "rank":
+                System.out.println(st.rank(split[1]));
+                break;
+            case "deleteMin":
+                st.deleteMin();
+                break;
+            case "contains":
+                System.out.println(st.contains(split[1]));
+                break;
+            case "keys":
+                System.out.println(st);
+                break;
+            case "get":
+                System.out.println(st.get(split[1]));
+                break;
+            default:
+                break;
+            }
+        }
+    }
+}
